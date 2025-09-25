9b115a772dcf6ac41d852d717e01f669c53c68a5
TayeburRahman <tayebrayhan101+@gmail.com>
2025-09-25 15:38:25 +0600

    updates all

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -233,7 +233,10 @@ const forgotPass = async (payload: { email: string }) => {
     { _id: 1, role: 1, email: 1, name: 1 }
   ) as IAuth;
 
-  if (!user.email) {
+  if (!user) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "User does not exist!");
+  }
+  if (!user?.email) {
     throw new ApiError(httpStatus.BAD_REQUEST, "User does not found!");
   }
 
