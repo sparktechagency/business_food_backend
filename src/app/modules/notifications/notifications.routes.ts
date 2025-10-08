1b4f52ebf0ace5c5c973b8772f8bcebe8403fc51
TayeburRahman <tayebrayhan101+@gmail.com>
2025-10-08 13:30:29 +0600

    updates all

--- src/app/modules/notifications/notifications.routes.ts
+++ src/app/modules/notifications/notifications.routes.ts
@@ -5,7 +5,6 @@ import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
-
 router.get(
   '/get-all-notifications',
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
