7a2b28908bca6549fa5490e27b28dd4f3ea0d4b0
TayeburRahman <tayebrayhan101+@gmail.com>
2026-01-18 11:39:43 +0600

    updates all

--- src/helpers/jwtHelpers.ts
+++ src/helpers/jwtHelpers.ts
@@ -3,25 +3,25 @@ import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 const createToken = (
   payload: Record<string, unknown>,
   secret: Secret,
-  expireTime: string,
+  expireTime: string | number,
 ): string => {
-  return jwt.sign(payload, secret, {
+  return jwt.sign(payload, secret as string, {
     expiresIn: expireTime,
-  });
+  } as any);
 };
 const createResetToken = (
   payload: any,
   secret: Secret,
-  expireTime: string,
+  expireTime: string | number,
 ): string => {
-  return jwt.sign(payload, secret, {
+  return jwt.sign(payload, secret as string, {
     algorithm: 'HS256',
     expiresIn: expireTime,
-  });
+  } as any);
 };
 
 const verifyToken = (token: string, secret: Secret): JwtPayload => {
-  return jwt.verify(token, secret) as JwtPayload;
+  return jwt.verify(token, secret as string) as JwtPayload;
 };
 
 export const jwtHelpers = {
